337e44fb7ef3804974c8ea378938e9574979600c
田村 亮弥 <tamuraryoya@LAB-N540.local>
2017-09-01 14:32:55 +0900

    app4

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ var app2 = new Vue({
         message: 'You loaded this page on ' + new Date().toLocaleString()
     }
 })
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+})
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ]
+    }
+})
