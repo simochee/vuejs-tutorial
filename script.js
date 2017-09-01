3a71ec2fcf294e7d92e2d07c1a1215e6001169db
田村 亮弥 <tamuraryoya@LAB-N540.local>
2017-09-01 14:42:56 +0900

    app7

--- script.js
+++ script.js
@@ -48,3 +48,19 @@ var app6 = new Vue({
         message: 'Hello Vue.js'
     }
 })
+
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+        groceryList: [
+            { id: 0, text: 'Vegetables' },
+            { id: 1, text: 'Cheese' },
+            { id: 2, text: 'Whatever else humans are supporsed to eat' }
+        ]
+    }
+})
